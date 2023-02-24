3171dee8153c7a03f33ee8798396aceef3cde79a
Smoky777 <tsft63@protonmail.com>
2023-02-25 00:53:27 +0100

    sixth commit

--- Form1.cs
+++ Form1.cs
@@ -8,8 +8,9 @@ namespace Crypter
 {
     public partial class Form1 : Form
     {
-        private byte[] key = ASCIIEncoding.ASCII.GetBytes("thisismyfuckikey");
-        private byte[] IV = ASCIIEncoding.ASCII.GetBytes("thisismytestkeys");
+        Byte[] key = ASCIIEncoding.ASCII.GetBytes("thisismyfuckikey");
+        Byte[] IV = ASCIIEncoding.ASCII.GetBytes("thisismytestkeys");
+
 
         public Form1()
         {
@@ -26,27 +27,19 @@ namespace Crypter
             }
         }
 
+
         private void BtnCrypt_Click(object sender, EventArgs e)
         {
             string inputFile = TxtExe.Text;
             string outputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "encrypted.exe");
-            string inputFileHash = ComputeFileHash(inputFile); // Calcul du hash du fichier
             EncryptFile(inputFile, outputFile);
 
-            string outFileHash = ComputeFileHash(outputFile);
-            if (inputFileHash != outFileHash)
-            {
-                MessageBox.Show("Le fichier a été modifié après son chiffrement !");
-                return;
-            }
-
-            MessageBox.Show("Le fichier a été chiffré et vérifié avec succès.");
-
-            System.Diagnostics.Process.Start(outputFile);
+            string inFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "encrypted.exe");
+            string outFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "decrypted.exe");
+            DecryptFile(inFile, outFile);
 
             MessageBox.Show("Encryption Done");
         }
-
 
         public void EncryptFile(string inputFile, string outputFile)
         {
@@ -57,18 +50,9 @@ namespace Crypter
                     RijndaelManaged aes = new RijndaelManaged();
                     aes.Key = key;
                     aes.IV = IV;
-                    // Écrire la clé et le vecteur d'initialisation au début du fichier
-                    fsEncrypted.Write(key, 0, key.Length);
-                    fsEncrypted.Write(IV, 0, IV.Length);
                     using (CryptoStream cs = new CryptoStream(fsEncrypted, aes.CreateEncryptor(), CryptoStreamMode.Write))
                     {
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = fsInput.Read(buffer, 0, buffer.Length)) > 0)
-                        {
-                            cs.Write(buffer, 0, bytesRead);
-                        }
-                        cs.FlushFinalBlock();
+                        fsInput.CopyTo(cs);
                     }
                 }
             }
@@ -76,45 +60,20 @@ namespace Crypter
 
         public void DecryptFile(string inputFile, string outputFile)
         {
-            // Lire la clé de chiffrement du début du fichier
-            byte[] key = new byte[16];
-            byte[] iv = new byte[16];
             using (FileStream fsInput = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
             {
-                fsInput.Read(key, 0, key.Length);
-                fsInput.Read(iv, 0, iv.Length);
-            }
-            using (FileStream fsInput = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
-            {
-                // Ignorer les 32 premiers octets (la clé et le vecteur d'initialisation)
-                fsInput.Seek(32, SeekOrigin.Begin);
-                using (FileStream fsDecrypted = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+                using (FileStream fsEncrypted = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
                 {
                     RijndaelManaged aes = new RijndaelManaged();
                     aes.Key = key;
-                    aes.IV = iv;
-                    using (CryptoStream cs = new CryptoStream(fsInput, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    aes.IV = IV;
+                    using (CryptoStream cs = new CryptoStream(fsEncrypted, aes.CreateDecryptor(), CryptoStreamMode.Write))
                     {
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = cs.Read(buffer, 0, buffer.Length)) > 0)
-                        {
-                            fsDecrypted.Write(buffer, 0, bytesRead);
-                        }
+                        fsInput.CopyTo(cs);
                     }
                 }
             }
         }
-
-
-        public string ComputeFileHash(string filePath)
-        {
-            using (var stream = new BufferedStream(File.OpenRead(filePath), 100000))
-            {
-                var sha256 = new SHA256Managed();
-                byte[] hash = sha256.ComputeHash(stream);
-                return BitConverter.ToString(hash).Replace("-", string.Empty);
-            }
-        }
     }
+
 }
