d11cef8351593ddb4a59915507926aa904ff4e8d
Smoky777 <tsft63@protonmail.com>
2023-02-25 00:15:27 +0100

    sixth commit

--- Form1.cs
+++ Form1.cs
@@ -8,9 +8,8 @@ namespace Crypter
 {
     public partial class Form1 : Form
     {
-        Byte[] key = ASCIIEncoding.ASCII.GetBytes("thisismyfuckikey");
-        Byte[] IV = ASCIIEncoding.ASCII.GetBytes("thisismytestkeys");
-
+        private byte[] key = ASCIIEncoding.ASCII.GetBytes("thisismyfuckikey");
+        private byte[] IV = ASCIIEncoding.ASCII.GetBytes("thisismytestkeys");
 
         public Form1()
         {
@@ -27,19 +26,27 @@ namespace Crypter
             }
         }
 
-
         private void BtnCrypt_Click(object sender, EventArgs e)
         {
             string inputFile = TxtExe.Text;
             string outputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "encrypted.exe");
+            string inputFileHash = ComputeFileHash(inputFile); // Calcul du hash du fichier
             EncryptFile(inputFile, outputFile);
 
-            string inFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "encrypted.exe");
-            string outFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "decrypted.exe");
-            DecryptFile(inFile, outFile);
+            string outFileHash = ComputeFileHash(outputFile);
+            if (inputFileHash != outFileHash)
+            {
+                MessageBox.Show("Le fichier a été modifié après son chiffrement !");
+                return;
+            }
+
+            MessageBox.Show("Le fichier a été chiffré et vérifié avec succès.");
+
+            System.Diagnostics.Process.Start(outputFile);
 
             MessageBox.Show("Encryption Done");
         }
+
 
         public void EncryptFile(string inputFile, string outputFile)
         {
@@ -50,9 +57,18 @@ namespace Crypter
                     RijndaelManaged aes = new RijndaelManaged();
                     aes.Key = key;
                     aes.IV = IV;
+                    // Écrire la clé et le vecteur d'initialisation au début du fichier
+                    fsEncrypted.Write(key, 0, key.Length);
+                    fsEncrypted.Write(IV, 0, IV.Length);
                     using (CryptoStream cs = new CryptoStream(fsEncrypted, aes.CreateEncryptor(), CryptoStreamMode.Write))
                     {
-                        fsInput.CopyTo(cs);
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = fsInput.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            cs.Write(buffer, 0, bytesRead);
+                        }
+                        cs.FlushFinalBlock();
                     }
                 }
             }
@@ -60,20 +76,45 @@ namespace Crypter
 
         public void DecryptFile(string inputFile, string outputFile)
         {
+            // Lire la clé de chiffrement du début du fichier
+            byte[] key = new byte[16];
+            byte[] iv = new byte[16];
             using (FileStream fsInput = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
             {
-                using (FileStream fsEncrypted = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+                fsInput.Read(key, 0, key.Length);
+                fsInput.Read(iv, 0, iv.Length);
+            }
+            using (FileStream fsInput = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            {
+                // Ignorer les 32 premiers octets (la clé et le vecteur d'initialisation)
+                fsInput.Seek(32, SeekOrigin.Begin);
+                using (FileStream fsDecrypted = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
                 {
                     RijndaelManaged aes = new RijndaelManaged();
                     aes.Key = key;
-                    aes.IV = IV;
-                    using (CryptoStream cs = new CryptoStream(fsEncrypted, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    aes.IV = iv;
+                    using (CryptoStream cs = new CryptoStream(fsInput, aes.CreateDecryptor(), CryptoStreamMode.Read))
                     {
-                        fsInput.CopyTo(cs);
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = cs.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fsDecrypted.Write(buffer, 0, bytesRead);
+                        }
                     }
                 }
             }
         }
-    }
 
+
+        public string ComputeFileHash(string filePath)
+        {
+            using (var stream = new BufferedStream(File.OpenRead(filePath), 100000))
+            {
+                var sha256 = new SHA256Managed();
+                byte[] hash = sha256.ComputeHash(stream);
+                return BitConverter.ToString(hash).Replace("-", string.Empty);
+            }
+        }
+    }
 }
--- KeyIV.Designer.cs
+++ KeyIV.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Crypter.Properties {
+namespace Crypter {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace Crypter.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    public class KeyIV {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal KeyIV() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Crypter.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Crypter.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Crypter.KeyIV", typeof(KeyIV).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
